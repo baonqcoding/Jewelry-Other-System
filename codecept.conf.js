9b639e29364e4c9fea810077dade47fa4e424a14
baobanpho <baobanpho@gmail.com>
2026-08-30 21:45:10 +0700

    Fix browser config in codecept

--- codecept.conf.js
+++ codecept.conf.js
@@ -1,18 +1,18 @@
 /** @type {CodeceptJS.MainConfig} */
 export const config = {
-  tests: './tests/*_test.js',
-  output: './output',
+  tests: "./tests/*_test.js",
+  output: "./output",
   helpers: {
     Playwright: {
-      browser: 'N',
-      url: 'http://localhost:8000',
-      show: true
-    }
+      browser: "chromium",
+      url: "http://localhost:8000",
+      show: true,
+    },
   },
   include: {
-    I: './steps_file.js'
+    I: "./steps_file.js",
   },
   noGlobals: true,
   plugins: {},
-  name: 'website_django'
-}
+  name: "website_django",
+};
